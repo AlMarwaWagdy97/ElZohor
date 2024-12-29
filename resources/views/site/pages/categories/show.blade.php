a6fa59d70d49e5a224c924a237bdd02608d2c34f
basma123456 <flowerbook100@gmail.com>
2024-12-29 16:49:11 +0200

    edit in index categories and show

--- resources/views/site/pages/categories/show.blade.php
+++ resources/views/site/pages/categories/show.blade.php
@@ -157,12 +157,12 @@
             max-height: 90%;
             overflow-y: hidden;
             text-align: center;
-            background-color: rgba(0, 0, 0, 0.6) !important;
+            background-color: rgba(0, 0, 0, 0.4) !important;
             position: absolute;
-            bottom: 5%;
+            bottom: 10%;
             left: 25%;
             padding: 1rem;
-            border: 3px solid var(--main-bg-purple-color);
+            border: 5px solid var(--main-bg-purple-color);
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
 
